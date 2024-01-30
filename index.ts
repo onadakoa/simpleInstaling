b722e126f3fb4d7ba03725e08b19a358b308b0b4
onadakoa <onadakoa@gmail.com>
2024-01-30 20:38:02 +0100

    start button clicking fix

--- index.ts
+++ index.ts
@@ -1,148 +1,164 @@
-import puppeteer, {Browser, ElementHandle, Page} from "puppeteer"
-import user from "./config"
+import puppeteer, { Browser, ElementHandle, Page } from "puppeteer";
+import user from "./config";
 
 let startButton = "#continue_session_button";
 
-let window : any;
+let window: any;
 
 const check_display = function (el: any) {
-    return el.style.display == "none";
+  return el.style.display == "none";
 };
 // finish: finish_page
 (async () => {
-  const browser = await puppeteer.launch({headless: user.headless})
-  const page = await browser.newPage()
+  const browser = await puppeteer.launch({ headless: user.headless });
+  const page = await browser.newPage();
 
-  await page.goto("https://instaling.pl/teacher.php?page=login")
+  await page.goto("https://instaling.pl/teacher.php?page=login");
 
   // await page.waitForNavigation()
   await delay(500);
   await delay(5000);
 
   let email = await page.waitForSelector("#log_email");
-  await email?.type(user.login)
-  let password = await page.waitForSelector("#log_password")
-  await password?.type(user.password)
-  let sub = await page.waitForSelector("button[type=submit]" )
-  await sub?.click()
+  await email?.type(user.login);
+  let password = await page.waitForSelector("#log_password");
+  await password?.type(user.password);
+  let sub = await page.waitForSelector("button[type=submit]");
+  await sub?.click();
 
-
-  await page.waitForNavigation()
+  await page.waitForNavigation();
   await delay(200);
 
   let url = await page.url().slice("https://instaling.pl/".length);
   if (!url.startsWith("student")) {
-    console.error("Wrong password/login!\nor server error?")
+    console.error("Wrong password/login!\nor server error?");
 
     browser.close();
     return;
   }
 
-  console.log("waiting for .btn-session selector")
+  console.log("waiting for .btn-session selector");
   await delay(200);
   // let start = await page.waitForSelector(".btn-session.btn.sesion.blink_me");
-  (await page.$(".btn-session.btn.sesion.blink_me"))?.click()
-  await delay(500)
+  let start = await page.$(".btn-session.btn.sesion.blink_me");
+  let link = await (await start?.getProperty("href"))?.jsonValue()
+
+  console.log(link)
+  page.goto(link as string)
+  await delay(500);
   // console.log(await start!.getProperty("href"))
   // await start!.click()
-  console.log("start")
-
+  console.log("start");
 
   // await delay(500);
-  await page.waitForNavigation({waitUntil: "load", timeout: 3000}).catch(err => {
-    console.error("couldn't wait for navigation")
-  })
+  await page.waitForNavigation({ waitUntil: "load", timeout: 3000 }).catch(
+    (err) => {
+      console.error("couldn't wait for navigation");
+    },
+  );
 
-  
+  while (
+    await page.$eval(
+      "#loading",
+      `!(${check_display.toString()})(document.querySelector('#loading'))`,
+    )
+  ) {} // loading screen secure
 
-  while (await page.$eval("#loading", `!(${check_display.toString()})(document.querySelector('#loading'))`)){} // loading screen secure
+  let ssp = await page.$eval(
+    "#start_session_page",
+    `(${check_display.toString()})(document.querySelector('#start_session_page'))`,
+  );
+  let csp = await page.$eval(
+    "#continue_session_page",
+    `(${check_display.toString()})(document.querySelector('#continue_session_page'))`,
+  );
 
-  let ssp = await page.$eval("#start_session_page", `(${check_display.toString()})(document.querySelector('#start_session_page'))`)
-  let csp = await page.$eval("#continue_session_page", `(${check_display.toString()})(document.querySelector('#continue_session_page'))`)
+  if (!ssp) startButton = "#start_session_button";
+  else startButton = "#continue_session_button";
 
-  if (!ssp) {startButton = "#start_session_button"}
-  else {startButton = "#continue_session_button"}
-
-
-  let start_session = await page.waitForSelector(startButton)
+  let start_session = await page.waitForSelector(startButton);
 
   await delay(500);
-  console.log("starting session")
-  await page.evaluate(`document.querySelector('${startButton}').click();`)
+  console.log("starting session");
+  await page.evaluate(`document.querySelector('${startButton}').click();`);
   await delay(100);
-  await page.waitForSelector(".translations")
+  await page.waitForSelector(".translations");
 
-  setTimeout(beginTest, 2000, page, browser)
+  setTimeout(beginTest, 2000, page, browser);
   //beginTest(page);
-})()
-
+})();
 
 interface word {
   translation: string;
   word: string;
 }
 
-function delay(ms: number) {return new Promise(res => setTimeout(res, ms))}
+function delay(ms: number) {
+  return new Promise((res) => setTimeout(res, ms));
+}
 
 async function getContent(element: ElementHandle) {
-  return (await (await element.getProperty("textContent")).toString()).slice("jshandle:".length)
+  return (await (await element.getProperty("textContent")).toString()).slice(
+    "jshandle:".length,
+  );
 }
 
 function getTranslation(translation: string, array: word[]): word | null {
   for (const i of array) {
     //console.log(`${i.translation} - ${translation} = ${i.translation==translation}`)
-    if (i.translation == translation) return i; 
+    if (i.translation == translation) return i;
   }
   return null;
 }
 
 async function beginTest(page: Page, browser: Browser) {
-  let word_list: word[] = []
+  let word_list: word[] = [];
 
-  let input = await page.waitForSelector("input#answer")
-
+  let input = await page.waitForSelector("input#answer");
 
   let i = 0;
-  let check_end = async () => await page.$eval("#finish_page", (el) => {return window.getComputedStyle(el).getPropertyValue("display") == "none"});
+  let check_end = async () =>
+    await page.$eval("#finish_page", (el) => {
+      return window.getComputedStyle(el).getPropertyValue("display") == "none";
+    });
   while (await check_end()) {
     i++;
     await delay(250);
     await page.waitForSelector("div.translations");
-    let translation = await page.$eval(".translations", el => el.innerHTML);
-    
-    console.log("loop: ", i)
+    let translation = await page.$eval(".translations", (el) => el.innerHTML);
+
+    console.log("loop: ", i);
 
     let wr = getTranslation(translation, word_list)!;
 
     if (wr != null) {
       await delay(100);
-      await input!.type(wr.word)  
-      console.log(`found ${wr.translation} is ${wr.word}`)
+      await input!.type(wr.word);
+      console.log(`found ${wr.translation} is ${wr.word}`);
 
-      await delay(1000)
+      await delay(1000);
       if (!await check_end()) continue;
-      await page.waitForSelector("div#check", {visible: true});
+      await page.waitForSelector("div#check", { visible: true });
       await (await page.$("div#check"))!.click();
     } else {
-      await delay(500)
+      await delay(500);
       await page.waitForSelector("div#check");
       await (await page.$("div#check"))!.click();
 
       await delay(200);
-      await page.waitForSelector("div#word")
-      let word = await page.$eval("div#word", el => el.innerHTML)
+      await page.waitForSelector("div#word");
+      let word = await page.$eval("div#word", (el) => el.innerHTML);
 
-      word_list.push({translation: translation, word: word})
+      word_list.push({ translation: translation, word: word });
     }
     await delay(1000);
-    console.log("check_end: " + await check_end())
+    console.log("check_end: " + await check_end());
     if (!await check_end()) continue;
-    await page.waitForSelector("div#nextword", {visible: true})
+    await page.waitForSelector("div#nextword", { visible: true });
     await (await page.$("div#nextword"))!.click();
   }
 
-
-  console.log(word_list)
-  page.close()
-  browser.close()
+  console.log(word_list);
+  page.close();
+  browser.close();
 }
